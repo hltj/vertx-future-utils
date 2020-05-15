03d93b4564cf1bd0abd6a044d607c94da3ae90b3
hltj <jiaywe@gmail.com>
2020-05-15 19:31:16 +0800

    TDD: add test cases for unimplemented new overload methods of `defaultWith()` & `fallbackWith()`

--- src/main/java/me/hltj/vertx/FutureUtils.java
+++ src/main/java/me/hltj/vertx/FutureUtils.java
@@ -5,7 +5,10 @@ import io.vertx.core.Future;
 import io.vertx.core.Handler;
 import io.vertx.core.Promise;
 
+import java.util.Optional;
 import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Supplier;
 
 /**
  * Convenient Utils for Vert.x Future
@@ -38,6 +41,18 @@ public final class FutureUtils {
     }
 
     /**
+     * if a Future succeed with null, map it with the default value
+     *
+     * @param future   the Future
+     * @param supplier a supplier to get the default value
+     * @param <T>      the type parameter of the Future
+     * @return the result Future
+     */
+    public static <T> Future<T> defaultWith(Future<T> future, Supplier<T> supplier) {
+        throw new RuntimeException("unimplemented");
+    }
+
+    /**
      * if a Future failed or succeed with null, replace it with a Future that succeed with the default value
      *
      * @param future the Future
@@ -47,5 +62,30 @@ public final class FutureUtils {
      */
     public static <T> Future<T> fallbackWith(Future<T> future, T v0) {
         return defaultWith(future.otherwise(v0), v0);
+    }
+
+    /**
+     * if a Future failed or succeed with null, replace it with a Future that succeed with the default value
+     *
+     * @param future   the Future
+     * @param function a function to get the default value
+     * @param <T>      the type parameter of the Future
+     * @return the result Future
+     */
+    public static <T> Future<T> fallbackWith(Future<T> future, Function<Optional<Throwable>, T> function) {
+        throw new RuntimeException("unimplemented");
+    }
+
+    /**
+     * if a Future failed or succeed with null, replace it with a Future that succeed with the default value
+     *
+     * @param future   the Future
+     * @param mapper   a function to get the default value on failure
+     * @param supplier a function to get the default value for replacing null
+     * @param <T>      the type parameter of the Future
+     * @return the result Future
+     */
+    public static <T> Future<T> fallbackWith(Future<T> future, Function<Throwable, T> mapper, Supplier<T> supplier) {
+        throw new RuntimeException("unimplemented");
     }
 }
--- src/test/java/test/me/hltj/vertx/FutureUtilsTest.java
+++ src/test/java/test/me/hltj/vertx/FutureUtilsTest.java
@@ -8,10 +8,13 @@ import lombok.SneakyThrows;
 import me.hltj.vertx.FutureUtils;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 import java.util.concurrent.CountDownLatch;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 class FutureUtilsTest {
 
@@ -54,10 +57,99 @@ class FutureUtilsTest {
     }
 
     @Test
+    void defaultWith_supplier() {
+        Set<Integer> numbers = new HashSet<>();
+
+        assertSucceedWith("value", FutureUtils.<String>defaultWith(Future.succeededFuture("value"), () -> {
+            numbers.add(0);
+            return "default";
+        }));
+        assertFalse(numbers.contains(0));
+
+        assertSucceedWith("default", FutureUtils.<String>defaultWith(Future.succeededFuture(), () -> {
+            numbers.add(1);
+            return "default";
+        }));
+        assertTrue(numbers.contains(1));
+
+        assertFailedWith("error", FutureUtils.<String>defaultWith(Future.failedFuture("error"), () -> {
+            numbers.add(2);
+            return "default";
+        }));
+        assertFalse(numbers.contains(2));
+    }
+
+    @Test
     void fallbackWith() {
         assertSucceedWith("value", FutureUtils.fallbackWith(Future.succeededFuture("value"), "fallback"));
         assertSucceedWith("fallback", FutureUtils.fallbackWith(Future.succeededFuture(), "fallback"));
         assertSucceedWith("fallback", FutureUtils.fallbackWith(Future.failedFuture("error"), "fallback"));
+    }
+
+    @Test
+    void fallbackWith_function() {
+        List<Throwable> throwables = new ArrayList<>();
+        Set<Integer> numbers = new HashSet<>();
+
+        assertSucceedWith("value", FutureUtils.<String>fallbackWith(Future.succeededFuture("value"), opt -> {
+            opt.ifPresent(throwables::add);
+            numbers.add(0);
+            return "fallback";
+        }));
+        assertTrue(throwables.isEmpty());
+        assertFalse(numbers.contains(0));
+
+        assertSucceedWith("fallback", FutureUtils.<String>fallbackWith(Future.succeededFuture(), opt -> {
+            opt.ifPresent(throwables::add);
+            numbers.add(1);
+            return "fallback";
+        }));
+        assertTrue(throwables.isEmpty());
+        assertTrue(numbers.contains(1));
+
+        assertSucceedWith("fallback", FutureUtils.<String>fallbackWith(Future.failedFuture("error"), opt -> {
+            opt.ifPresent(throwables::add);
+            numbers.add(2);
+            return "fallback";
+        }));
+        assertFalse(throwables.isEmpty());
+        assertTrue(numbers.contains(2));
+    }
+
+    @Test
+    void fallbackWith_mapper_supplier() {
+        List<Throwable> throwables = new ArrayList<>();
+        Set<Integer> numbers = new HashSet<>();
+
+        assertSucceedWith("value", FutureUtils.fallbackWith(Future.succeededFuture("value"), t -> {
+            throwables.add(t);
+            return "otherwise";
+        }, () -> {
+            numbers.add(0);
+            return "default";
+        }));
+        assertTrue(throwables.isEmpty());
+        assertFalse(numbers.contains(0));
+
+        assertSucceedWith("default", FutureUtils.fallbackWith(Future.succeededFuture(), t -> {
+            throwables.add(t);
+            return "otherwise";
+        }, () -> {
+            numbers.add(1);
+            return "default";
+        }));
+        assertTrue(throwables.isEmpty());
+        assertTrue(numbers.contains(1));
+
+        assertSucceedWith("otherwise", FutureUtils.fallbackWith(Future.failedFuture("error"), t -> {
+            throwables.add(t);
+            return "otherwise";
+        }, () -> {
+            numbers.add(2);
+            return "default";
+        }));
+        assertFalse(throwables.isEmpty());
+        assertFalse(numbers.contains(2));
     }
 
     private <T> void assertSucceedWith(T expected, Future<T> actual) {
